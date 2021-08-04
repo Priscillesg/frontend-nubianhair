ae0f8309eae6da8022e4ee4f6ffd63a3bad04e6a
Priscille Gwet <ngomakak@gmail.com>
2021-08-03 22:03:26 -0500

    Installed Axios
    Fetched data from the api backend to get a list of businesses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
+// import axios from 'axios';
 import BusinessesList from './components/BusinessesList';
+import { useState , useEffect} from 'react';
+
+
+
+
+
 
 function App() {
+  const [businesses, setBusinesses] = useState([])
+
+  useEffect( () => {
+    fetch('http://127.0.0.1:8000/api_list/', {
+      'method':'GET',
+      headers: {
+        'Content-Type':'application/json',
+        'Authorization':`Token 164db5e00610c5a682f19e61ec0960f656de73b2` 
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => setBusinesses(resp["businesses"]))
+    .catch(error => console.log(error))
+
+  }, []);
+
   return (
     <div className="App">
-        <BusinessesList/>
+        {businesses.map(business => {
+          return <h2>{business["name"]}</h2>
+        })}
+      <BusinessesList/>
     </div>
   );
 }
